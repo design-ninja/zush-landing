98c0ed05a3afd2414930ed0b219644a22b98385b
lirik <kirill.isachenko@gmail.com>
2026-08-08 07:40:54 +0700

    fix: sync Windows app version

--- src/constants.ts
+++ src/constants.ts
@@ -24,7 +24,7 @@ export const SOFTPEDIA_REVIEW_URL =
 
 export const FREE_DOWNLOAD_BADGE_LABEL = "FREE";
 export const MAC_APP_VERSION = "3.9.1";
-export const WINDOWS_APP_VERSION = "3.8.0.0";
+export const WINDOWS_APP_VERSION = "3.8.1.0";
 export const PRODUCT_FACTS_REVIEWED_AT = "2026-08-05";
 
 export interface AppRating {
